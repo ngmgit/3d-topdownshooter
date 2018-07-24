d85a7c34d55cdf544d588fc12fcce84389404ec1
Nishanth Mane <nishanth.engg93@gmail.com>
2018-07-24 21:53:43 +0530

    * Set zombie scale in model file * Adjust zombie prefab scale values in prefab for above changes * Adjust camera setttings to perspective and y values reduced.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour {
 	public Vector3 m_cameraOffsets;
 
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
 		transform.position = m_target.position + m_cameraOffsets;
 	}
 }
